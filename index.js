cc06f8d38a67ef1e3f5a721f57495a6094ca6782
dikshant8875 <101505078+dikshant8875@users.noreply.github.com>
2022-04-12 11:44:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 const HOST = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Dikshant the Nodejs app is working fine')
 })
 
 app.listen(port, () => {
